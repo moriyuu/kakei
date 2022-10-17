7030e2525473c5d505a9ac8c3b5575724d756b67
Yuta Mori <moriyu54321@gmail.com>
2022-10-18 02:38:38 +0900

    setup microcms and swr

---
+++ src/lib/microcms.ts
@@ -0,0 +1,8 @@
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: "kakei",
+  apiKey: "8776cc24ae7a4f5591f453280dbef748fe56",
+});
+
+export default client;
