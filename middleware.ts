d74643aa65f54da64b33c90abaf98c909dc1ca16
Yuta Mori <moriyu54321@gmail.com>
2022-10-29 19:57:03 +0900

    nextjs logger middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const logger = (request: NextRequest) => {
+  const url = new URL(request.url);
+  if (url.pathname.startsWith("/api")) {
+    console.log("nextjs", request.method, request.url, request.body);
+  }
+};
+
+export function middleware(request: NextRequest) {
+  logger(request);
+
+  return NextResponse.next();
+}
