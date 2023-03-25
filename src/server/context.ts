ae8853ad8eb5dafdcbba8dc4915f2c43aaa07bb3
Yuta Mori <moriyu54321@gmail.com>
2023-03-25 18:09:37 +0900

    wip

--- src/server/context.ts
+++ src/server/context.ts
@@ -2,8 +2,10 @@ import { inferAsyncReturnType } from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
 export async function createContext(opts: trpcNext.CreateNextContextOptions) {
+  const idToken =
+    (opts.req.headers["authorization"]?.toString() ?? "").split(" ")[1] || null;
   return {
-    session: "foo", // TODO:
+    idToken,
   };
 }
 
