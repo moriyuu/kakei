c6172dcf85345d1d34eedfb1d6201adeecd6b4ea
Yuta Mori <moriyu54321@gmail.com>
2023-03-29 14:39:02 +0900

    wip

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,31 @@
 import { Hono } from "hono";
 import { poweredBy } from "hono/powered-by";
 
-const app = new Hono();
+export interface Env {
+  DB: D1Database;
+}
+
+const app = new Hono<{ Bindings: Env }>();
 
 app.use("*", poweredBy());
 
 app.get("/", (c) => {
   return c.text("Hello Hono !!");
+});
+
+app.get("/hello", async (c) => {
+  const companyName = c.req.query("company");
+  console.log("companyName", companyName);
+  const { results } = await c.env.DB.prepare(
+    "SELECT * FROM Customers WHERE CompanyName = ?"
+  )
+    .bind(companyName ?? "")
+    .all();
+
+  return c.json({
+    ok: true,
+    results,
+  });
 });
 
 export default app;
